9a2a28a0a505383fd6af95ee4e1edf5e3b028686
Charles Tapley Hoyt <cthoyt@gmail.com>
2020-06-25 12:49:56 +0200

    Fix networkx intersphinx mapping

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -57,7 +57,7 @@ texinfo_documents = [
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3', None),
-    'networkx': ('https://networkx.github.io/', None),
+    'networkx': ('https://networkx.github.io/documentation/latest/', None),
     'sqlalchemy': ('https://docs.sqlalchemy.org/en/latest', None),
     'pybel': ('https://pybel.readthedocs.io/en/latest/', None),
 }
